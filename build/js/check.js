114e5a5f381a88f0a64d7cb88e1d076f9ea17424
Evgeniy Levashov <evgenii.levashov@gmail.com>
2016-05-25 22:40:15 +0300

    debug

--- build/js/check.js
+++ build/js/check.js
@@ -2,28 +2,24 @@
 var getMessage = function (a, b) {
 
   if (typeof a === 'boolean') {
-    if (a === true) {
+    if (a) {
       return 'Переданное GIF-изображение анимировано и содержит ' + b + ' кадров';
     } else {
       return 'Переданное GIF-изображение не анимировано';
     }
-  }
-
-  if (typeof a === 'number') {
+  } else if (typeof a === 'number') {
     return 'Переданное SVG-изображение содержит ' + a + ' объектов и ' + (b * 4) + ' атрибутов';
-  }
-
-  if (Array.isArray(a)) {
-    for (var i=0; i < a.length; i++) {
-      var sum += a[i];
-        return 'Количество красных точек во всех строчках изображения: ' + sum;
-    }
-  }
-
-  if (Array.isArray(a) && Array.isArray(b)) {
-    for (var i=0; i < a.length; i++) {
-      var square += a[i] * b[i];
+  } else if (Array.isArray(a)) {
+    if (Array.isArray(b)) {
+      for (var i=0; i < a.length; i++) {
+        var square += a[i] * b[i];
         return 'Общая площадь артефактов сжатия: ' + square + ' пикселей';
+      }
+    } else {
+      for (var i=0; i < a.length; i++) {
+        var sum += a[i];
+        return 'Количество красных точек во всех строчках изображения: ' + sum;
+      }
     }
   }
 };
