ce6eec2e94ccaf20cebe94bc9e349cc58cfdac69
Evgeniy Levashov <evgenii.levashov@gmail.com>
2016-05-26 09:13:53 +0300

    Перенес переменные square, sum в начало функции

--- build/js/check.js
+++ build/js/check.js
@@ -1,5 +1,7 @@
 'use strict';
 var getMessage = function (a, b) {
+  
+  var square = 0, sum = 0;
 
   if (typeof a === 'boolean') {
     if (a) {
@@ -12,12 +14,12 @@ var getMessage = function (a, b) {
   } else if (Array.isArray(a)) {
     if (Array.isArray(b)) {
       for (var i=0; i < a.length; i++) {
-        var square += a[i] * b[i];
+        square += a[i] * b[i];
         return 'Общая площадь артефактов сжатия: ' + square + ' пикселей';
       }
     } else {
       for (var i=0; i < a.length; i++) {
-        var sum += a[i];
+        sum += a[i];
         return 'Количество красных точек во всех строчках изображения: ' + sum;
       }
     }
